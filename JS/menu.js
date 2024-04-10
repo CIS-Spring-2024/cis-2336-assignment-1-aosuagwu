a273caf34752b61df395e63e78a6a604d8210644
aosuagwu <aosuagwu@cougarnet.uh.edu>
2024-04-10 13:27:44 -0500

    Added some js

---
+++ JS/menu.js
@@ -0,0 +1,12 @@
+const menuItems = [
+    { name: 'Pancakes', image: '/IMAGES/pancakes.webp', description: 'Fluffy Buttermilk Pancakes' },
+    { name: 'Breakfast Tacos', image: '/IMAGES/breakfast tacos.jpg', description: 'Savory Breakfast Tacos' },
+    { name: 'Oatmeal', image: '/IMAGES/oatmeal.jpg', description: 'Sweet Oats' },
+    { name: 'Omelette', image: '/IMAGES/omelette.jpg', description: 'Healthy Omelette' },
+    { name: 'Sandwich', image: '/IMAGES/sandwich.jpg', description: 'Grilled Chicken Sandwich' },
+    { name: 'Garden Salad', image: '/IMAGES/salad.jpg', description: 'Fresh Garden Salad' },
+    { name: 'Pizza', image: '/IMAGES/pizza.jpg', description: 'Pepperoni Pizza' },
+    { name: 'Pasta', image: '/IMAGES/pasta.jpg', description: 'Tomato Cream Pasta' },
+    { name: 'Fried Rice', image: '/IMAGES/fried_rice.jpg', description: 'Tasty Fried Rice' },
+    { name: 'Steak', image: '/IMAGES/steak.jpg', description: 'Tender Steak and Fresh Brocolli' }
+]
