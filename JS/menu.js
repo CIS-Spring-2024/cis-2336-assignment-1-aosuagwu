405b24004e11dd3c2e657c4a18db1f67b80d8470
aosuagwu <aosuagwu@cougarnet.uh.edu>
2024-04-17 15:17:27 -0500

    Added more style

--- JS/menu.js
+++ JS/menu.js
@@ -37,3 +37,25 @@ function renderMenuItems(category, containerId) {
 renderMenuItems('breakfast', 'breakfastMenu');
 renderMenuItems('lunch', 'lunchMenu');
 renderMenuItems('dinner', 'dinnerMenu');
+
+function submitOrder() {
+    const items = document.querySelectorAll('.menu-item');
+    const order = [];
+    let isValid = true;
+    
+    items.forEach(item => {
+        const itemName = item.querySelector('.menu-item-name').textContent;
+        const quantity = parseInt(item.querySelector('.quantity').value);
+
+        if (isNaN(quantity) || quantity <= 0){
+            alert('Please enter a valid quantity for ${itemName}');
+            isValid = false;
+            return;
+        }
+        order.push({ name: itemName, quantity: quantity});
+    });
+    if (isValid){
+        console.log(order);
+        alert('Order has been successfully submitted!')
+    }
+}
